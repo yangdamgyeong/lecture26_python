73837cca43d1b0185551bc881447874bae758291
yangdamgyeong <yangd0324@gmail.com>
2026-05-19 13:29:37 +0900

    Add files via upload

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -4,21 +4,29 @@ class Hangman:
     MASK_CHAR = '_'
     RIGHT = 1
     WRONG = 0
-    EXIST = -1
+    ERROR = -1
     WIN = 1
     LOOSE = 0
     CONTINUE = -1
     def __init__(self, word_list):
-        self.word = random.choice(word_list)
+        self.word = random.choice(word_list).upper()
         self.display_word = Hangman.MASK_CHAR * len(self.word)
         self.num_try = 0
+        self.input_letters = []
+        self.error_status = ""
     def check_letter(self, letter):
         letter = letter.upper()
+        #이미 입력했던 문자인지 확인
+        if letter in self.input_letters:
+            self.error_status = f'이미 입력한 알파벳입니다. {self.input_letters}'
+            return Hangman.ERROR
+        
+        self.input_letters.append(letter)
         if self.word.count(letter) > 0: 
             #있는 문자이면 display_word에서 위치를 찾아 수정
             for i in range(len(self.word)):
                 if self.word[i] == letter:
-                    self.display_word = self.display_word[:1] + letter + self.display_word[i+1:]
+                    self.display_word = self.display_word[:i] + letter + self.display_word[i+1:]
             return Hangman.RIGHT
         else:
             self.num_try += 1
--- Hangman/hangman_console.py
+++ Hangman/hangman_console.py
@@ -1,16 +1,37 @@
 from hangman import Hangman
+import re
+
+def load_word_list():
+    word_list = []
+    with open('HangMan/voca.txt', 'rt', encoding='utf-8')as f:
+        for voca in f:
+            voca = voca.lstrip()
+            if not voca:
+                continue
+            match = re.match(r'^[a-zA-A]+', voca)
+            if match:
+                pure_word = match.group()
+                remaining_text = voca[len(pure_word):]
+                if remaining_text and re.match(r'^\s', remaining_text):
+                    if not re.search(r'[a-zA-Z]', remaining_text):
+                        word_list.append(pure_word)
+
+    return word_list
 
 print()
 print('============ Hangman =============')
-word_list = ['apple', 'banana', 'man', 'woman', 'tomato']
+
 
 #단어 선택
-hangman = Hangman(word_list)
+hangman = Hangman(load_word_list())
 print(f'{hangman.display_word}, ({len(hangman.word)}글자)')
 
 while True:
     # 알파벳 입력
     letter = input('>> 알파벳 입력 : ')
+    if not letter.isalpha():
+        print('알파벳을 입력하세요.')
+        continue
     
     # 정답 확인
     result = hangman.check_letter(letter)
@@ -18,6 +39,9 @@ while True:
         print(f'정답 : {hangman.display_word}')
     elif result == Hangman.WRONG:
         print(f'오답 : {hangman.num_try}회 시도')
+    else:
+        print(hangman.error_status)
+        continue
 
     # 승패확인
     result = hangman.is_win()
@@ -27,4 +51,7 @@ while True:
     elif result == Hangman.LOOSE:
         print(f'YOU LOOSE~~ : {hangman.word}')
         break
+
+
+
 
