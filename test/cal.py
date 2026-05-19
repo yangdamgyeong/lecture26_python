116d33dbc7fe44bc7fbe6c1e1f21228ff998f69d
yangdamgyeong <yangd0324@gmail.com>
2026-05-19 13:29:58 +0900

    Add files via upload

---
+++ test/cal.py
@@ -0,0 +1,8 @@
+# 계산기 모듈
+def add(a, b):
+    return a + b
+def sub(a, b):
+    return a - b
+
+if __name__ == '__main__': # 최상위 파일이다
+    print('cal,py 테스트 : add(1,2) -> ', add(1, 2))
---
+++ test/console_cal.py
@@ -0,0 +1,3 @@
+import cal
+a, b = map(int(input('더할 두 수를 입력 : ')).split())
+print('두 수의 합 : ', cal.add(a, b))
